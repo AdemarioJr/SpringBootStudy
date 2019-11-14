f23ad53c3b89addd98c438c9485f93a0cf613799
ademario <ademariofernandesramos@gmail.com>
2019-11-14 00:59:37 -0300

    Aplicação impl no Intellij - Teste Versionamento - 01

--- forum/src/main/java/com/example/demo/controller/HomeController.java
+++ forum/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,17 +1,15 @@
-package br.com.caelum.forum.controller;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-	@ResponseBody
-	public String index() {
-		return "Bem vindo ao curso Forum Caelum -->";
-	}
-
+    @ResponseBody
+    public String index() {
+        return "Bem vindo ao curso Forum Caelum--->>";
+    }
 }
--- forum/src/test/java/com/example/demo/Demo03ApplicationTests.java
+++ forum/src/test/java/com/example/demo/Demo03ApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.caelum.forum;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Demo03ApplicationTests {
 
 	@Test
 	void contextLoads() {
